2fcd0978473c1ead2aa488c99f1baa8e5fa5b0ea
psripac <psriprac@yahoo.com>
2020-05-25 11:09:09 -0700

    added tinymce4

--- website/homepage/admin.py
+++ website/homepage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Sample
+
 # Register your models here.
+
+class SampleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Title/date', {'fields': ['sample_title', 'sample_published']}),
+        ('Content', {'fields': ['sample_content']})
+    ]
+
+admin.site.register(Sample, SampleAdmin)
--- website/homepage/models.py
+++ website/homepage/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Sample(models.Model):
     sample_title = models.CharField(max_length=200)
     sample_content = models.TextField()
-    sample_published = models.DateTimeField('date published')
+    sample_published = models.DateTimeField('date published', default=datetime.now())
 
     def __str__(self):
         return self.sample_title
