1fb3bce078b3db68da6c9184496314500099d14d
psripac <psriprac@yahoo.com>
2020-05-25 10:53:50 -0700

    routed urls and created sample model

--- website/homepage/models.py
+++ website/homepage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Sample(models.Model):
+    sample_title = models.CharField(max_length=200)
+    sample_content = models.TextField()
+    sample_published = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.sample_title
